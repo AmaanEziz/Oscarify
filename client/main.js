129723dce5604a6dbde53907a257874169b94127
reaganhoang <reaganhoang99@gmail.com>
2020-11-02 23:06:42 -0800

    add content for front-page

---
+++ client/main.js
@@ -0,0 +1,20 @@
+(function(document) {
+	'use strict';
+
+	var LightTableFilter = (function(Arr) {
+
+		var _input;
+
+		function _onInputEvent(e) {
+			_input = e.target;
+			var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+			Arr.forEach.call(tables, function(table) {
+				Arr.forEach.call(table.tBodies, function(tbody) {
+					Arr.forEach.call(tbody.rows, _filter);
+				});
+			});
+		}
+		
+	});
+
+})(document);
