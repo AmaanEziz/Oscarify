637d2b5336e7ddb5e40bd0a57168c35c43eb63c9
reaganhoang <reaganhoang99@gmail.com>
2020-11-16 20:13:12 -0800

    add API call to get movies search object print out

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ app.get('/', function(req, res){
     res.render('search');
 });
 
+app.get('/result', function(req, res){
+    var query = req.query.search;
+    var url = 'https://www.omdbapi.com/?s=' + query + '&apikey=35711dae';
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body)
+            console.log(data);
+            res.render('result', {data: data});
+        }
+    });
+});
+
+
+
  app.listen(8080, function(){
      console.log('Movie app started on port: 8080');
  });
